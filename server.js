141fffcf1c9fe4d0d36102ebc4732128bae9c20f
Adarshmihir <franck431@hotmail.fr>
2018-03-05 09:12:39 +0100

    Add files via upload
    server.js Airy

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require("express");
+var express = require("express");       
 var app = express();
-var MongoClient = require("mongodb").MongoClient;
+var mongoClient = require("mongodb").MongoClient;
 
 
 // connexion à la bdd 
 var db;
-MongoClient.connect('mongodb://localhost:27017/gayale',
-    function(err,_db)  {
+mongoClient.connect('mongodb://localhost:27017/gayale',
+    function(err, _db)  {
         if (err)
-            console.log("Erreur de connexion à mongodb");
+            console.log("Erreur de connexion à mongoDB");
         else {
             console.log("Yeah! Connected!");
             db = _db;
@@ -20,16 +20,31 @@ app.use("/css", express.static(__dirname + "/html/css"))
 .use("/images", express.static(__dirname + "/images"))
 .use("/js", express.static(__dirname + "/html/js"))
 
-.get("/", function (req,res) {
-	res.sendFile(__dirname + "/html/index.html");
+/***** Routes toutes pages sauf celle du user *****/
+.get('/', function (req, res) {
+    res.sendFile(__dirname + "/html/requalificationTicket.html");
+})
+
+.get("/gestion", function (req,res) {
+	res.sendFile(__dirname + "/html/login.html");
 })
 
 .get("/message", function(req,res){
 	db.collection("messages").find().toArray(function(err,msg){
 		res.json({ message: msg[0].text });
 	});
-});
+})
 
-app.listen(8093);
+.post("/gestion/connect", function(req, res) {
+    res.sendFile(__dirname + "/html/creaVisuRapport.html");
+})
 
+.post("/gestion/connect/liste", function(req, res) {
+    res.sendFile(__dirname + "/html/listeTickets.html");
+})
 
+.post("/gestion/connect/test",function(req, res) {
+    res.sendFile(__dirname + "/html/gestionTicket.html");
+})
+
+app.listen(8142);
